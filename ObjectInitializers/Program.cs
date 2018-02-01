0751cd522e090c1240875295672326d60b285a87
0l3shka <q0l3shka@gmail.com>
2018-02-01 15:58:04 +0700

    Test object init syntax

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -10,6 +10,19 @@ namespace ObjectInitializers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Init Syntax ******\n");
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.ReadLine();
+          
         }
     }
 }
