4a44e4ff3b20e99c49ce98a3db5162c68f6b39fe
0l3shka <q0l3shka@gmail.com>
2018-02-01 15:04:15 +0700

    Test Get/Set Name methods

--- EmployeeApp/Employee.cs
+++ EmployeeApp/Employee.cs
@@ -8,5 +8,42 @@ namespace EmployeeApp
 {
     class Employee
     {
+        //variables
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        public Employee() { }
+        public Employee(string name,int id,float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        //methods
+        public string GetName()
+        {
+            return empName;
+        }
+        public void SetName(string name)
+        {
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name must be less than 16 chars!");
+            else
+                empName = name;
+        }
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID:{0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+
+        }
+        
     }
 }
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -10,6 +10,14 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("**** Fun with Encapsulation ******\n");
+            Employee emp = new Employee("Marvin", 456, 30000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+            Console.ReadLine();
         }
     }
 }
